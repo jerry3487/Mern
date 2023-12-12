024cb3d63d60535930f6c08d3f3c158b688e2c7d
Damodaran D <ddamodaran14@gmail.com>
2023-12-12 09:58:05 +0530

    updated react project

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,102 @@
+import { useEffect, useReducer, useState } from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import '../index.css'
+import Product from "../components/Product";
+import { Helmet } from "react-helmet-async";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+// import data from '../data';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function HomeScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: "",
+  });
+  const [currentImage, setCurrentImage] = useState(0);
+  const [images, setImages] = useState([
+    "images/offer.jpg", // Replace with your image URLs
+    "images/offer1.jpg",
+  ]);
+  const nextImage = () => {
+    setCurrentImage((prev) => (prev + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImage((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextImage();
+    }, 10000); 
+
+    return () => clearInterval(interval);
+  }, []);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <Helmet>
+        <title>Mozeerat</title>
+      </Helmet>
+      <div className="image-slider">
+        <div className="slider-dots">
+          {images.map((_, index) => (
+            <div
+              key={index}
+              className={`dot ${currentImage === index ? "active" : ""}`}
+              onClick={() => setCurrentImage(index)}
+            ></div>
+          ))}
+        </div>
+        <img src={images[currentImage]} alt={`Banner ${currentImage + 1}`} />
+      </div>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <LoadingBox />
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
